bb4bf0e67099a639d7871f36dea2de37d10cf83e
Kris <kapsona@yahoo.com>
2020-03-29 12:08:51 -0700

    First Online Store

---
+++ src/homepage.component.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./homepage.style.scss";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtititle">Shop Now</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtititle">Shop Now</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNEAKERS</h1>
+            <span className="subtititle">Shop Now</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMENs</h1>
+            <span className="subtititle">Shop Now</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MENS</h1>
+            <span className="subtititle">Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
