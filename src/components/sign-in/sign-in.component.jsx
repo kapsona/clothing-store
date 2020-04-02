1ee16b3a8e39f8b226fb1623fd67e08b946521f1
Kris <kapsona@yahoo.com>
2020-04-02 11:12:49 -0700

    Login with email and password implemented

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,16 +4,24 @@ import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { SignInWithGoogle } from "../../firebase/firebase.util";
+import { auth, SignInWithGoogle } from "../../firebase/firebase.util";
+
 class SignIn extends Component {
   constructor(props) {
     super(props);
     this.state = { email: "", password: "" };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      // Display login error message here
+    }
   };
 
   handleChange = event => {
